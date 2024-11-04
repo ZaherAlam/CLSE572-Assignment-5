8c705901498cd7d3a30010e3f0830f0ffc7d7312
Zaher Alam <148729962+ZaherAlam@users.noreply.github.com>
2024-11-04 14:43:18 -0500

    Create assignment5.sh

---
+++ assignment5.sh
@@ -0,0 +1,9 @@
+my_lipophilicity_model/
+├── my_model_package/
+│   ├── __init__.py
+│   └── train_model.py
+├── data/
+│   └── lipophilicity.csv
+├── README.md
+├── requirements.txt
+└── .gitignore
